41049bd2cd1777db9065ee1cea61fa1954647a48
react521 <1417003485@qq.com>
2021-02-21 15:43:42 +0800

    add createComponents

---
+++ src/components/Header/index.vue
@@ -0,0 +1,55 @@
+<!--  -->
+<template>
+  <header id="header">
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "喵喵电影",
+    },
+  },
+  components: {},
+  data() {
+    //这里存放数据
+    return {};
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+};
+</script>
+<style lang="scss" scoped>
+#header {
+  width: 100%;
+  height: 50px;
+  color: #fff;
+  background: #e54847;
+  border-bottom: 1px solid #e54847;
+  position: relative;
+}
+#header h1 {
+  font-size: 18px;
+  text-align: center;
+  line-height: 50px;
+  font-weight: normal;
+}
+#header i {
+  position: absolute;
+  left: 5px;
+  top: 50%;
+  margin-top: -13px;
+  font-size: 26px;
+}
+</style>
---
+++ src/components/Login/index.vue
@@ -0,0 +1,76 @@
+<!--  -->
+<template>
+  <div class="login_body">
+    <div>
+      <input class="login_text" type="text" placeHolder="账户名/手机号/Email" />
+    </div>
+    <div>
+      <input class="login_text" type="password" placeHolder="请输入您的密码" />
+    </div>
+    <div class="login_btn">
+      <input type="submit" value="登录" />
+    </div>
+    <div class="login_link">
+      <a href="#">立即注册</a>
+      <a href="#">找回密码</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  name: "CiList",
+  components: {},
+  data() {
+    //这里存放数据
+    return {};
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+};
+</script>
+<style lang="scss" scoped>
+#content .login_body {
+  width: 100%;
+}
+.login_body .login_text {
+  width: 100%;
+  height: 40px;
+  border: none;
+  border-bottom: 1px #ccc solid;
+  margin-bottom: 5px;
+  outline: none;
+  text-indent: 10px;
+}
+.login_body .login_btn {
+  height: 50px;
+  margin: 10px;
+}
+.login_body .login_btn input {
+  width: 100%;
+  height: 100%;
+  background: #e54847;
+  border-radius: 3px;
+  border: none;
+  display: block;
+  color: white;
+  font-size: 20px;
+}
+.login_body .login_link {
+  display: flex;
+  justify-content: space-between;
+}
+.login_body .login_link a {
+  text-decoration: none;
+  margin: 0 5px;
+  font-size: 12px;
+  color: #e54847;
+}
+</style>
